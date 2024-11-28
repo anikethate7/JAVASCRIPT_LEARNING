78476267c3e865fcb3b6758cef3a91923265d306
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-11-28 12:32:56 +0530

    added delete task functionality

--- Todo_List/script.js
+++ Todo_List/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  tasks.forEach(task => renderTask(task));
+  tasks.forEach((task) => renderTask(task));
 
   addTaskButton.addEventListener("click", () => {
     const taskText = todoInput.value.trim();
@@ -19,11 +19,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
     tasks.push(newTask);
     saveTasks();
+    renderTask(newTask);
     todoInput.value = "";
     console.log(tasks);
   });
 
-  function renderTask(task) {}
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>Delete</button>
+    `;
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+    todoList.appendChild(li);
+  }
 
   function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
