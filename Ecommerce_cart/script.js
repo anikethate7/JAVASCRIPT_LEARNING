79d33c6362098ea1d605dd4aadb4e600cf379890
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-12-04 22:12:26 +0530

    init

---
+++ Ecommerce_cart/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const product = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 10.99,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 15.99,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 20.99,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 20.99,
+    }
+  ];
+
+  const cart = [];
+
+  const productList = document.getElementById("product-list");
+
+  product.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+      <span>${product.name} - $${product.price.toFixed(2)}</span>
+      <button data-id="${product.id}">Add to cart</button>`;
+    productList.appendChild(productDiv);
+  });
+
+  productList.addEventListener("click", (e) => {
+    if(e.target.tagName === "BUTTON") return;
+  });
+});
