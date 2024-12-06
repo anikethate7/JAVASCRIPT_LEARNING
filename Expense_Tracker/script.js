fa35b51744e065e0b5bdaa93e78977237b74cca9
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-12-06 23:03:40 +0530

    added expense amount rendering

--- Expense_Tracker/script.js
+++ Expense_Tracker/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseNameInput = document.getElementById("expense-name");
   const expenseAmountInput = document.getElementById("expense-amount");
   const expenseList = document.getElementById("expense-list");
-  const totalAmount = document.getElementById("total-amount");
+  const totalAmountDisplay = document.getElementById("total-amount");
 
   let expenses = [];
   let totalAmountValue = calculateTotalAmount();
@@ -11,10 +11,32 @@ document.addEventListener("DOMContentLoaded", () => {
   expenseForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const expenseName = expenseNameInput.value.trim();
-    const expenseAmount = expenseAmountInput.value;
-    console.log(typeof expenseAmount);
-    
+    const expenseAmount = parseFloat(expenseAmountInput.value);
+
+    if (expenseName !== "" && !isNaN(expenseAmount) && expenseAmount > 0) {
+      const newExpense = {
+        id: Date.now,
+        name: expenseName,
+        amount: expenseAmount,
+      };
+      expenses.push(newExpense);
+      saveExpensesToLocal();
+      updateTotal();
+
+      expenseNameInput.value = "";
+      expenseAmountInput.value = "";
+    }
   });
 
-  function calculateTotalAmount() {}
+  function updateTotal(){
+    totalAmountValue = calculateTotalAmount();
+    totalAmountDisplay.textContent = totalAmountValue.toFixed(2);
+  }
+  function calculateTotalAmount() {
+    const intialvalue = 0;
+    return expenses.reduce((sum,expense) => sum +expense.amount,0)
+  }
+  function saveExpensesToLocal() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
 });
